4c12890b58d9c973516c10866c16255c43baa8a9
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-01-18 19:58:32 +0300

    main component cleaning

--- airbnb/src/Main.js
+++ airbnb/src/Main.js
@@ -36,17 +36,3 @@ const Main = () => {
 
 export default Main;
 //  <Contact />
-
-// <img src={Katie} alt="katie" />
-//       <div className="rating">
-//         <div className="stats">
-//           <img src={Star} alt="star" />
-//           <span>5.0</span>
-//           <span className="gray">(6) .</span>
-//           <span className="gray">USA</span>
-//         </div>
-//         <p>Life lessons with Katie Zaferes</p>
-//         <p>
-//           <span style={{ fontWeight: "bold" }}>From $136 </span>/ person
-//         </p>
-//       </div>
