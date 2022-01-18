38b836c30a984b10dd85524894815ee308d712ef
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-01-18 19:57:35 +0300

    Using props in the card component

--- airbnb/src/Card.js
+++ airbnb/src/Card.js
@@ -1,21 +1,21 @@
 import React from "react";
 import Star from "./images/Star.png";
-import Katie from "./images/image 12.png";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
-      <img src={Katie} alt="katie" />
+      <img className="photo" src={props.image} alt="katie" />
       <div className="rating">
         <div className="stats">
           <img src={Star} alt="star" />
-          <span>5.0</span>
-          <span className="gray">(6) .</span>
-          <span className="gray">USA</span>
+          <span>{props.stats.rating}</span>
+          <span className="gray">({props.stats.voters}) .</span>
+          <span className="gray">{props.stats.country}</span>
         </div>
-        <p>Life lessons with Katie Zaferes</p>
+        <p>{props.lesson}</p>
         <p>
-          <span style={{ fontWeight: "bold" }}>From $136 </span>/ person
+          <span style={{ fontWeight: "bold" }}>From $ {props.price} </span>/
+          person
         </p>
       </div>
     </div>
--- airbnb/src/Main.js
+++ airbnb/src/Main.js
@@ -2,15 +2,33 @@ import React from "react";
 import Card from "./Card";
 // import Contact from "./Contact";
 import Home from "./Home";
+import Katie from "./images/image 12.png";
+import Wedding from "./images/wedding-photography 1.png";
+import Mountain from "./images/mountain-bike 1.png";
 
 const Main = () => {
   return (
     <div className="main">
       <Home />
       <div className="cards-main">
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          image={Katie}
+          lesson="Life lessons with Katie Zaferes"
+          price={136}
+          stats={{ rating: 5.0, voters: 6, country: "USA" }}
+        />
+        <Card
+          image={Wedding}
+          lesson="Learn wedding photography"
+          price={125}
+          stats={{ rating: 4.8, voters: 15, country: "UK" }}
+        />
+        <Card
+          image={Mountain}
+          lesson="Group Mountain Biking"
+          price={50}
+          stats={{ rating: 4.7, voters: 37, country: "Russia" }}
+        />
       </div>
     </div>
   );
@@ -18,3 +36,17 @@ const Main = () => {
 
 export default Main;
 //  <Contact />
+
+// <img src={Katie} alt="katie" />
+//       <div className="rating">
+//         <div className="stats">
+//           <img src={Star} alt="star" />
+//           <span>5.0</span>
+//           <span className="gray">(6) .</span>
+//           <span className="gray">USA</span>
+//         </div>
+//         <p>Life lessons with Katie Zaferes</p>
+//         <p>
+//           <span style={{ fontWeight: "bold" }}>From $136 </span>/ person
+//         </p>
+//       </div>
